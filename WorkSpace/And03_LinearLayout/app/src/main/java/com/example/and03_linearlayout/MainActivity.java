fab595bf62ec5cea419e62538c3e5bb55f869a6e
youngmoon525 <youngmoon525@naver.com>
2022-11-16 13:04:32 +0900

    adsf

---
+++ WorkSpace/And03_LinearLayout/app/src/main/java/com/example/and03_linearlayout/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.and03_linearlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        android.util.Log.d("필터", "onCreate: ");
+
+    }
+}
